ffb08ca7ce9239991c182e5c76e54c414d56beb3
Jennifer Nguyen <jenniferngp001@gmail.com>
2024-04-08 16:31:26 -0700

    Added How_to_GEO and made analyses directory

---
+++ How_to_GEO.sh
@@ -0,0 +1,29 @@
+# 1. Organize data in a single directory with sub-directories corresponding to each data type (ATAC, RNA, etc.)
+# Important: GEO said to NOT upload metadata through FTP!!
+
+# 2. When data is ready to upload, log onto to GEO: https://www.ncbi.nlm.nih.gov/geo/submitter/
+
+# 3. Go to "GEO submissions" or "My submissions" in upper-right taskbar
+
+# 4. Click "New submission"
+
+# 5. Click "Submit high-throughput sequencing"
+
+# 6. Under the "Uploading your submission" section, click "Transfer Files".
+
+# 7. On the terminal in flh1, open a screen session.
+screen
+
+# 8. Upload data onto GEO using lftp. You will need the following info:
+# the GEO-provided password under "Step 2", bullet point f
+# the GEO-provided upload space under "Step 1"
+# The path to your submission directory
+
+lftp ftp://geoftp:[password]>@ftp-private.ncbi.nlm.nih.gov
+cd [upload space]
+mirror -R [path_to_submission_directory]
+
+# 9. Exit screen using Ctrl+A+D
+
+# 10. To go back to your screen session to check progress
+screen -R [screen_session_id]
